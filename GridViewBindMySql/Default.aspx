5ecc650efacf1ed7be97ccbc650fc58a6f8cccb8
dkpreet <dkpreet.k@gmail.com>
2016-03-27 16:37:54 -0400

    Project starting changed
    The project starting point has been changed, because the previous master
    page was having many toubles

---
+++ GridViewBindMySql/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GridViewBindMySql._Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <section class="featured">
+        <div class="content-wrapper">
+            <hgroup class="title">               
+                <h1>Automated Timetable Builder
+                </h1>
+            </hgroup>
+        </div>
+    </section>
+</asp:Content>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h3>The courses available are: </h3>
+    <asp:Panel ID="Panel1" runat="server" Width="100%" ScrollBars="Horizontal">
+    <asp:GridView ID="grvCustomers" runat="server" AutoGenerateColumns="false">
+        <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <AlternatingRowStyle BackColor="#BFE4FF" />
+        <Columns>
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <asp:CheckBox ID="chkCtrl" runat="server" />
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:BoundField DataField="Course_ID" HeaderText="Course_ID" ItemStyle-Width="90" />
+            <asp:BoundField DataField="Course_Code" HeaderText="Course_Code" ItemStyle-Width="120" />
+            <asp:BoundField DataField="Course_Name" HeaderText="Course_Name" ItemStyle-Width="90" />
+            <asp:BoundField DataField="Section_ID" HeaderText="Section_ID" ItemStyle-Width="100" />
+            <asp:BoundField DataField="Max_Seat" HeaderText="Max_Seat" ItemStyle-Width="150" />
+        </Columns>
+    </asp:GridView>
+    <br />
+    <asp:Button ID="btnBind" runat="server" Text="Show selected courses" OnClick="btnBind_Click" />
+    <br /><br />
+    <asp:Label ID="lblmsg" runat="server" />
+    </asp:Panel>
+</asp:Content>
---
+++ GridViewBindMySql/Default.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//Using namespaces 
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace GridViewBindMySql
+{
+    public partial class _Default : Page
+    {
+        #region MySqlConnection Connection
+        MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Page.IsPostBack)
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand("select * from course", conn);
+                    MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    adp.Fill(ds);
+                    adp.Dispose();
+                    cmd.Dispose();
+                    conn.Close();
+                    grvCustomers.DataSource = ds.Tables[0];
+                    grvCustomers.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
+        }
+        #endregion
+        #region show message
+        /// <summary>
+        /// This function is used for show message.
+        /// </summary>
+        /// <param name="msg"></param>
+        void ShowMessage(string msg)
+        {
+            ClientScript.RegisterStartupScript(Page.GetType(), "validation", "<script language='javascript'>alert('" + msg + "');</script>");
+        }
+        #endregion
+        #region Bind Data
+        /// <summary>
+        /// This display the data fetched from the table using MySQLCommand,DataSet and MySqlDataAdapter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnBind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data = "";
+                foreach (GridViewRow row in grvCustomers.Rows)
+                {
+                    if (row.RowType == DataControlRowType.DataRow)
+                    {
+                        CheckBox chkRow = (row.Cells[0].FindControl("chkCtrl") as CheckBox);
+                        if (chkRow.Checked)
+                        {
+                            string Course_ID = row.Cells[1].Text;
+                            string Course_Code = row.Cells[2].Text;
+                            string Course_Name = row.Cells[3].Text;
+                            string Section_ID = row.Cells[4].Text;
+                            string Max_Seat = row.Cells[5].Text;
+
+                            data = data + Course_ID + ",   " + Course_Code + ",   " + Course_Name + ",   " + Section_ID + ",   " + Max_Seat + "<br>";
+                        }
+                    }
+                }
+                lblmsg.Text = data;
+
+            }
+            catch (MySqlException ex)
+            {
+                ShowMessage(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+               
+            }
+            //btnBind.Visible = false;
+        }
+        #endregion
+    }
+}
